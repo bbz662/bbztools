ed8b8f4e825318c30ef37a338800acb30521960d
bbz662 <hiroshi.babazono@kufu.co.jp>
2024-08-18 22:05:47 +0900

    fix _redirects

--- _worker.js
+++ _worker.js
@@ -1,17 +1,11 @@
-export default {
-    async fetch(request, env) {
-        const url = new URL(request.url);
-        if (url.hostname.endsWith('bbztools.pages.dev')) {
-            const newUrl = new URL(url);
-            newUrl.hostname = 'tools.bbz662.com';
-            return Response.redirect(newUrl.toString(), 301);
-        }
-        //   if (url.hostname.endsWith('.pages.dev')) {
-        //     const newUrl = new URL(url);
-        //     newUrl.hostname = 'bbz662.com';
-        //     return Response.redirect(newUrl.toString(), 301);
-        //   }
-
-        return env.ASSETS.fetch(request);
-    }
-};
+// export default {
+//     async fetch(request, env) {
+//         const url = new URL(request.url);
+//         if (url.hostname.endsWith('bbztools.pages.dev')) {
+//             const newUrl = new URL(url);
+//             newUrl.hostname = 'tools.bbz662.com';
+//             return Response.redirect(newUrl.toString(), 301);
+//         }
+//         return env.ASSETS.fetch(request);
+//     }
+// };
